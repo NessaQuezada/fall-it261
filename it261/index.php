ca31b2bd7c0b8f0793385370d25e5bfbb72547c7
NessaQuezada <122851420+NessaQuezada@users.noreply.github.com>
2024-09-30 20:09:29 -0700

    Add files via upload

---
+++ it261/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My portal page</title>
+    <link href="css/styles.css" type="text/css"
+    rel="stylesheet">
+</head>
+<body>
+    <h1><a href="index.php">Vanessa's portal page</a></h1>
+    <h2>The navigation below represents our BIG Assignment.</h2>
+  <div id="wrapper">
+<nav>
+    <ul>
+        <li><a href="">Switch</a></li>
+        <li><a href="">Troubleshoot</a></li>
+        <li><a href="">Calculator</a></li>
+        <li><a href="">Email</a></li>
+        <li><a href="">Database</a></li>
+        <li><a href="">Gallery</a></li>
+    </ul>
+</nav>
+
+<main>
+<h2>About Vanessa</h2>
+<img class="right" src="images/nessa.jpg" alt="vanessa">
+<p>A paragraph about yourself, ie., goals, background, hobbies etc! Semper ex sapien; natoque sapien efficitur efficitur pharetra rhoncus. Donec pulvinar magna nullam purus ridiculus. Nibh et montes varius tempor iaculis. Laoreet non sem odio orci vel luctus cras. Hac nam et viverra risus sollicitudin luctus lobortis? Urna diam consequat tellus vel neque. Elementum pharetra facilisis ad tristique metus suscipit lacus. Mollis pretium quam torquent blandit finibus purus. Nunc felis nullam sollicitudin venenatis euismod bibendum pretium netus.</p>
+
+</main>
+
+<aside>
+<h2>Weekly Class Exercises</h2>
+<h3>Week 2</h3>
+
+<ol>
+
+<li><a href="">var.php</a></li>
+<li><a href="">var2.php</a></li>
+<li><a href="">something.php</a></li>
+<li><a href="">somethingagain.php</a></li>
+
+</ol>
+
+</aside>
+
+</div>
+<!--close wrapper-->
+</body>
+</html>
