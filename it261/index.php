53861b33178f2314c1e60bcbbd460dee599b683b
NessaQuezada <122851420+NessaQuezada@users.noreply.github.com>
2024-10-07 20:30:45 -0700

    Add files via upload
    finished portal. start test

--- it261/index.php
+++ it261/index.php
@@ -8,11 +8,11 @@
     rel="stylesheet">
 </head>
 <body>
-    <h1><a href="index.php">Vanessa's portal page</a></h1>
-    <h2>The navigation below represents our BIG Assignment.</h2>
+
   <div id="wrapper">
 <nav>
     <ul>
+    <li><a href="">Home</a></li>
         <li><a href="">Switch</a></li>
         <li><a href="">Troubleshoot</a></li>
         <li><a href="">Calculator</a></li>
@@ -24,8 +24,12 @@
 
 <main>
 <h2>About Vanessa</h2>
-<img class="right" src="images/nessa.jpg" alt="vanessa">
-<p>A paragraph about yourself, ie., goals, background, hobbies etc! Semper ex sapien; natoque sapien efficitur efficitur pharetra rhoncus. Donec pulvinar magna nullam purus ridiculus. Nibh et montes varius tempor iaculis. Laoreet non sem odio orci vel luctus cras. Hac nam et viverra risus sollicitudin luctus lobortis? Urna diam consequat tellus vel neque. Elementum pharetra facilisis ad tristique metus suscipit lacus. Mollis pretium quam torquent blandit finibus purus. Nunc felis nullam sollicitudin venenatis euismod bibendum pretium netus.</p>
+<img class="left" src="images/nessie.jpg" alt="vanessa">
+<p>Hello, my name is Vanessa Quezada. I am a 25-year-old artist based in Seattle, Washington. Most days I focus on working with digital media, but I have skills with multiple mediums such as acrylic paintings, clay sculpting, Sewing, and more. Being an artist is a big part of my identity as I love to create and work with my hands. In my free time, I love reading, watching TV, and playing video games. My favorite video game right now is Hades as the character designs are interesting and it is a game with a plot but ultimately you can play it mindlessly, which is relaxing after a hard day.</p>
+<p>I have taken my love of art and video games and channeled them into web designing as there are ways to create web page video games with just HTML and CSS.</p>
+
+<img class="left" src="images/mamp1.png" alt="vanessa">
+<img class="left" src="images/mamp2.png" alt="vanessa">
 
 </main>
 
@@ -35,9 +39,10 @@
 
 <ol>
 
-<li><a href="">var.php</a></li>
-<li><a href="">var2.php</a></li>
-<li><a href="">something.php</a></li>
+<li><a href="weeks/week2/var.php">var.php</a></li>
+<li><a href="weeks/week2/vars2.php">vars2.php</a></li>
+<li><a href="weeks/week2/currency-logic.php">currency-logic</a></li>
+<li><a href="weeks/week2/currency.php">currency</a></li>
 <li><a href="">somethingagain.php</a></li>
 
 </ol>
@@ -46,5 +51,22 @@
 
 </div>
 <!--close wrapper-->
+
+<footer>
+    <ul>
+        <li>Copyright &copy;
+            2024</li>
+        <li>All Rights Reserved</li>
+        <li><a href="../index.php">Web Design by YOUR NAME</a></li>
+        <li><a id="html-checker" href="#">HTML Validation</a></li>
+        <li><a id="css-checker" href="#">CSS Validation</a></li>
+        </ul>
+        
+        <script>
+                document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+                document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+        </script>
+ </footer>
+
 </body>
 </html>
